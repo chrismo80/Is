df0ac705b3dbc16eef1bc6888bb64260deb16095
chrismo80 <cm@imstec.de>
2025-05-22 12:43:35 +0200

    Add line breaks to error messages

--- Is/MessageExtensions.cs
+++ Is/MessageExtensions.cs
@@ -4,24 +4,24 @@ using System.Collections;
 
 public static class MessageExtensions
 {
-	internal static string Actually(this object? actual, string equality, object? expected) =>
-		actual.Format() + " actually " + equality + " " + expected.Format();
+    internal static string Actually(this object? actual, string equality, object? expected) =>
+        "\n" + actual.Format() + "\n" + equality + "\n" + expected.Format() + "\n";
 
-	internal static string Format(this object? value) =>
-		value.FormatValue() + value.FormatType();
+    internal static string Format(this object? value) =>
+        value.FormatValue() + value.FormatType();
 
-	private static string FormatValue(this object? value) =>
-		value switch
-		{
-			null => "<NULL>",
-			string => $"\"{value}\"",
-			IEnumerable list => $"[{list.FormatArray()}]",
-			_ => $"{value}"
-		};
+    private static string FormatValue(this object? value) =>
+        value switch
+        {
+            null => "<NULL>",
+            string => $"\"{value}\"",
+            IEnumerable list => $"[{list.FormatArray()}]",
+            _ => $"{value}"
+        };
 
-	private static string FormatArray(this IEnumerable list) =>
-		string.Join("|", list.Cast<object>().Select(x => x.FormatValue()));
+    private static string FormatArray(this IEnumerable list) =>
+        string.Join("|", list.Cast<object>().Select(x => x.FormatValue()));
 
-	private static string FormatType(this object? value) =>
-		value is null or Type ? "" : $" ({value.GetType()})";
+    private static string FormatType(this object? value) =>
+        value is null or Type ? "" : $" ({value.GetType()})";
 }
