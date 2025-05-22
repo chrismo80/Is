f2a963ca04255c9a5f9c3a3cdd10bc00341d681c
chrismo80 <cm@imstec.de>
2025-05-22 08:33:12 +0200

    Add test case for enumerables with only one element

--- Is.Tests/Tests.cs
+++ Is.Tests/Tests.cs
@@ -2,160 +2,164 @@ namespace Is.Tests;
 
 public class Tests
 {
+    [Test]
+    [TestCase(null, null)]
+    [TestCase(false, false)]
+    [TestCase(true, true)]
+    [TestCase(1, 1)]
+    [TestCase(2.2, 2.2)]
+    [TestCase(3f, 3f)]
+    [TestCase("4", "4")]
+    public void Is_Actual_Equals_Expected(object? actual, object? expected) =>
+        actual.Is(expected);
 
-	[Test]
-	[TestCase(null, null)]
-	[TestCase(false, false)]
-	[TestCase(true, true)]
-	[TestCase(1, 1)]
-	[TestCase(2.2, 2.2)]
-	[TestCase(3f, 3f)]
-	[TestCase("4", "4")]
-	public void Is_Actual_Equals_Expected(object? actual, object? expected) =>
-		actual.Is(expected);
+    [Test]
+    [TestCase(null, true)]
+    [TestCase(null, false)]
+    [TestCase(true, false)]
+    [TestCase(5, null)]
+    [TestCase(6, 6d)]
+    [TestCase(7, true)]
+    [TestCase(false, 7d)]
+    [TestCase(8d, 8f)]
+    [TestCase(99, "99")]
+    [TestCase("ABC", false)]
+    [TestCase("ABC", null)]
+    [TestCase("ABC", "ABD")]
+    public void Actual_Not_Equals_Expected(object? actual, object? expected)
+    {
+        Action act = () => actual.Is(expected);
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	[TestCase(null, true)]
-	[TestCase(null, false)]
-	[TestCase(true, false)]
-	[TestCase(5, null)]
-	[TestCase(6, 6d)]
-	[TestCase(7, true)]
-	[TestCase(false, 7d)]
-	[TestCase(8d, 8f)]
-	[TestCase(99, "99")]
-	[TestCase("ABC", false)]
-	[TestCase("ABC", null)]
-	[TestCase("ABC", "ABD")]
-	public void Actual_Not_Equals_Expected(object? actual, object? expected)
-	{
-		Action act = () => actual.Is(expected);
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void ListValues_Equal_Expected() =>
+        VerifyEquality(new List<int> { 1, 2, 3, 4 });
 
-	[Test]
-	public void ListValues_Equal_Expected() =>
-		VerifyEquality(new List<int> { 1, 2, 3, 4 });
+    [Test]
+    public void ArrayValues_Equal_Expected() =>
+        VerifyEquality(new int[] { 1, 2, 3, 4 });
 
-	[Test]
-	public void ArrayValues_Equal_Expected() =>
-		VerifyEquality(new int[] { 1, 2, 3, 4 });
+    [Test]
+    public void ValuesWithNull_Equal_Expected() =>
+        new int?[] { 1, 2, null, 4 }.Is(1, 2, null, 4);
 
-	[Test]
-	public void ValuesWithNull_Equal_Expected() =>
-		new int?[] { 1, 2, null, 4 }.Is(1, 2, null, 4);
+    [Test]
+    public void List_Not_Equal_List()
+    {
+        Action act = () => new List<int?> { 1, 2, null, 4 }.Is(new List<int?> { 1, 2, 3, 4 });
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void List_Not_Equal_List()
-	{
-		Action act = () => new List<int?> { 1, 2, null, 4 }.Is(new List<int?> { 1, 2, 3, 4 });
-		act.IsThrowing<IsNotException>();
-	}
-	[Test]
-	public void Array_Not_Equal_Params()
-	{
-		Action act = () => new List<int> { 1, 2, 3, 5 }.Is(new List<int> { 1, 2, 3, 4 });
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void Array_Not_Equal_Params()
+    {
+        Action act = () => new List<int> { 1, 2, 3, 5 }.Is(new List<int> { 1, 2, 3, 4 });
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void IEnumerable_Not_Equal_Params_TooShort()
-	{
-		Action act = () => new List<int> { 1, 2, 3, 5 }.Where(i => i % 2 == 0).Is(2, 4);
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void IEnumerable_Not_Equal_Params_TooShort()
+    {
+        Action act = () => new List<int> { 1, 2, 3, 5 }.Where(i => i % 2 == 0).Is(2, 4);
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void IEnumerable_Not_Equal_Params_TooLong()
-	{
-		Action act = () => new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 2 == 0).Is(2, 4);
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void IEnumerable_Not_Equal_Params_TooLong()
+    {
+        Action act = () => new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 2 == 0).Is(2, 4);
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void JaggedArrays_Equals_Expected() =>
-		new object[] { new[] { 1, 2 }, 3 }.Is(new object[] { new[] { 1, 2 }, 3 });
+    [Test]
+    public void IEnumerable_Equal_Params()
+    {
+        new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 2 == 0).Is(2, 4, 6);
+        new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 3 == 0).Is(3, 6);
+        new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 4 == 0).Is(4);
+    }
 
-	[Test]
-	public void DifferentDepth_EqualsThough_Expected() =>
-		new List<object> { 1, 2 }.Is(new List<object> { 1, new List<object> { 2 } });
+    [Test]
+    public void JaggedArrays_Equals_Expected() =>
+        new object[] { new[] { 1, 2 }, 3 }.Is(new object[] { new[] { 1, 2 }, 3 });
 
-	[Test]
-	public void DifferentDepth_EqualsExactly_Fails()
-	{
-		Action act = () => new List<object> { 1, 2 }.IsExactly(new List<object> { 1, new List<object> { 2 } });
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void DifferentDepth_EqualsThough_Expected() =>
+        new List<object> { 1, 2 }.Is(new List<object> { 1, new List<object> { 2 } });
 
-	[Test]
-	public void Value_NotEquals_List()
-	{
-		Action act = () => 5.Is(new List<int> { 1, 2 });
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void DifferentDepth_EqualsExactly_Fails()
+    {
+        Action act = () => new List<object> { 1, 2 }.IsExactly(new List<object> { 1, new List<object> { 2 } });
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void Value_Is_Type() =>
-		new List<int>().Is<IReadOnlyList<int>>();
+    [Test]
+    public void Value_NotEquals_List()
+    {
+        Action act = () => 5.Is(new List<int> { 1, 2 });
+        act.IsThrowing<IsNotException>();
+    }
 
-	[Test]
-	public void Value_IsNot_Type()
-	{
-		Action act = () => new List<int>().Is<IReadOnlyList<double>>();
-		act.IsThrowing<IsNotException>();
-	}
+    [Test]
+    public void Value_Is_Type() =>
+        new List<int>().Is<IReadOnlyList<int>>();
 
-	[Test]
-	[TestCase(3, 4)]
-	[TestCase(5.7, 9.5)]
-	[TestCase(-4, -2)]
-	[TestCase(-1, 1)]
-	[TestCase(0, 5)]
+    [Test]
+    public void Value_IsNot_Type()
+    {
+        Action act = () => new List<int>().Is<IReadOnlyList<double>>();
+        act.IsThrowing<IsNotException>();
+    }
 
-	public void IsGreaterThan_IsSmallerThan<T>(T actual, T expected) where T : IComparable<T>
-	{
-		actual.IsSmallerThan(expected);
-		expected.IsGreaterThan(actual);
-	}
+    [Test]
+    [TestCase(3, 4)]
+    [TestCase(5.7, 9.5)]
+    [TestCase(-4, -2)]
+    [TestCase(-1, 1)]
+    [TestCase(0, 5)]
+    public void IsGreaterThan_IsSmallerThan<T>(T actual, T expected) where T : IComparable<T>
+    {
+        actual.IsSmallerThan(expected);
+        expected.IsGreaterThan(actual);
+    }
 
+    [Test]
+    public void Booleans()
+    {
+        true.IsTrue();
+        false.IsFalse();
+    }
 
-	[Test]
-	public void Booleans()
-	{
-		true.IsTrue();
-		false.IsFalse();
-	}
+    [Test]
+    public void IsEmpty()
+    {
+        var list = new List<int>();
+        list.IsEmpty();
+    }
 
+    [Test]
+    public void IsNull()
+    {
+        List<int>? list = null;
+        list.IsNull();
+    }
 
-	[Test]
-	public void IsEmpty()
-	{
-		var list = new List<int>();
-		list.IsEmpty();
-	}
+    [Test]
+    [TestCase(1.000001, 1.0)]
+    [TestCase(2.999999f, 3f)]
+    [TestCase(1000000.1, 1000000.0)]
+    [TestCase(1_000_000.0, 1_000_001.0)]
+    [TestCase(783.0123, 783.0124)]
+    public void IsCloseTo_Actual_Expected(object actual, object expected) =>
+        actual.Is(expected);
 
-
-	[Test]
-	public void IsNull()
-	{
-		List<int>? list = null;
-		list.IsNull();
-	}
-
-	[Test]
-	[TestCase(1.000001, 1.0)]
-	[TestCase(2.999999f, 3f)]
-	[TestCase(1000000.1, 1000000.0)]
-	[TestCase(1_000_000.0, 1_000_001.0)]
-	[TestCase(783.0123, 783.0124)]
-	public void IsCloseTo_Actual_Expected(object actual, object expected) =>
-		actual.Is(expected);
-
-	private static void VerifyEquality(IEnumerable<int> values)
-	{
-		values.Is(new int[] { 1, 2, 3, 4 });
-		values.Is(new List<int> { 1, 2, 3, 4 });
-		values.Is(1, 2, 3, 4);
-		values.Where(i => i % 2 == 0).Is(2, 4);
-	}
+    private static void VerifyEquality(IEnumerable<int> values)
+    {
+        values.Is(new int[] { 1, 2, 3, 4 });
+        values.Is(new List<int> { 1, 2, 3, 4 });
+        values.Is(1, 2, 3, 4);
+        values.Where(i => i % 2 == 0).Is(2, 4);
+    }
 }
--- Is/IsExtensions.cs
+++ Is/IsExtensions.cs
@@ -5,81 +5,84 @@ using System.Collections;
 
 public static class IsExtensions
 {
-	public static T IsThrowing<T>(this Action action) where T : Exception
-	{
-		try
-		{
-			action();
-		}
-		catch (Exception ex)
-		{
-			return ex.Is<T>();
-		}
+    public static T IsThrowing<T>(this Action action) where T : Exception
+    {
+        try
+        {
+            action();
+        }
+        catch (Exception ex)
+        {
+            return ex.Is<T>();
+        }
 
-		throw new IsNotException($"No {typeof(T)} was thrown");
-	}
+        throw new IsNotException($"No {typeof(T)} was thrown");
+    }
 
-	public static T Is<T>(this object actual) =>
-		actual is T cast ? cast : throw new IsNotException(actual.Actually("is no", typeof(T)));
+    public static T Is<T>(this object actual) =>
+        actual is T cast ? cast : throw new IsNotException(actual.Actually("is no", typeof(T)));
 
-	public static bool Is(this object actual, params object[]? expected) =>
-		actual.ShouldBe(expected?.Unwrap());
+    public static bool Is(this object actual, params object[]? expected) =>
+        actual.ShouldBe(expected?.Unwrap());
 
-	public static bool IsExactly(this object actual, object expected) =>
-		actual.IsEqualTo(expected);
+    public static bool IsExactly(this object actual, object expected) =>
+        actual.IsEqualTo(expected);
 
-	public static bool IsNull(this object actual) =>
-		actual.IsEqualTo(null);
+    public static bool IsNull(this object actual) =>
+        actual.IsEqualTo(null);
 
-	public static bool IsTrue(this bool actual) =>
-		actual.IsEqualTo(true);
+    public static bool IsTrue(this bool actual) =>
+        actual.IsEqualTo(true);
 
-	public static bool IsFalse(this bool actual) =>
-		actual.IsEqualTo(false);
+    public static bool IsFalse(this bool actual) =>
+        actual.IsEqualTo(false);
 
-	public static bool IsEmpty(this IEnumerable actual) =>
-		!actual.Cast<object>().Any() ? true
-			: throw new IsNotException($"{actual.Format()} actually is not empty");
+    public static bool IsEmpty(this IEnumerable actual) =>
+        !actual.Cast<object>().Any() ? true
+            : throw new IsNotException($"{actual.Format()} actually is not empty");
 
-	public static bool IsGreaterThan<T>(this T actual, T other) where T : IComparable<T> =>
-		actual.CompareTo(other) > 0 ? true
-			: throw new IsNotException(actual.Actually("is not greater than", other));
+    public static bool IsGreaterThan<T>(this T actual, T other) where T : IComparable<T> =>
+        actual.CompareTo(other) > 0 ? true
+            : throw new IsNotException(actual.Actually("is not greater than", other));
 
-	public static bool IsSmallerThan<T>(this T actual, T other) where T : IComparable<T> =>
-		actual.CompareTo(other) < 0 ? true
-			: throw new IsNotException(actual.Actually("is not smaller than", other));
+    public static bool IsSmallerThan<T>(this T actual, T other) where T : IComparable<T> =>
+        actual.CompareTo(other) < 0 ? true
+            : throw new IsNotException(actual.Actually("is not smaller than", other));
 
-	private static bool ShouldBe(this object actual, object[]? expected) =>
-		expected?.Length switch
-		{
-			null => actual.IsEqualTo(null),
-			1 => actual.IsEqualTo(expected[0]),
-			_ => actual.ToArray().Are(expected)
-		};
+    private static bool ShouldBe(this object actual, object[]? expected) =>
+        expected?.Length switch
+        {
+            null => actual.IsEqualTo(null),
+            1 when !actual.IsEnumerable() => actual.IsEqualTo(expected[0]),
+            _ => actual.ToArray().Are(expected)
+        };
 
-	private static object[] Unwrap(this object[] array) =>
-		array is [IEnumerable first and not string] ? first.ToArray() : array;
+    private static object[] Unwrap(this object[] array) =>
+        array.Length == 1 && array[0].IsEnumerable() ? array[0].ToArray() : array;
 
-	private static object[] ToArray(this object value) =>
-		Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
+    private static bool IsEnumerable(this object value) =>
+        value is IEnumerable and not string;
 
-	private static bool Are(this object[] values, object[] expected) =>
-		values.Length == expected.Length ? Enumerable.Range(0, expected.Length).All(i => values[i].Is(expected[i]))
-			: throw new IsNotException(values.Actually("are not", expected));
+    private static object[] ToArray(this object value) =>
+        Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
 
-	private static bool IsEqualTo<T>(this T? actual, T? expected) =>
-		EqualityComparer<T>.Default.Equals(actual, expected) || actual.IsCloseTo(expected) ? true
-			: throw new IsNotException(actual.Actually("is not", expected));
+    private static bool Are(this object[] values, object[] expected) =>
+        values.Length == expected.Length ? Enumerable.Range(0, expected.Length).All(i => values[i].Is(expected[i]))
+            : throw new IsNotException(values.Actually("are not", expected));
 
-	private static bool IsCloseTo<T>(this T? actual, T? expected) =>
-		(actual, expected) switch
-		{
-			(float a, float e) => a.IsInTolerance(e, 1e-6f),
-			(double a, double e) => a.IsInTolerance(e, 1e-6),
-			_ => false
-		};
+    private static bool IsEqualTo<T>(this T? actual, T? expected) =>
+        EqualityComparer<T>.Default.Equals(actual, expected) || actual.IsCloseTo(expected) ? true
+            : throw new IsNotException(actual.Actually("is not", expected));
 
-	private static bool IsInTolerance<T>(this T actual, T expected, T tolerance) where T : IFloatingPoint<T> =>
-		T.Abs(actual - expected) <= tolerance * T.Max(T.One, T.Abs(expected)) ? true
-			: throw new IsNotException(actual.Actually("is not close to", expected));
+    private static bool IsCloseTo<T>(this T? actual, T? expected) =>
+        (actual, expected) switch
+        {
+            (float a, float e) => a.IsInTolerance(e, 1e-6f),
+            (double a, double e) => a.IsInTolerance(e, 1e-6),
+            _ => false
+        };
+
+    private static bool IsInTolerance<T>(this T actual, T expected, T tolerance) where T : IFloatingPoint<T> =>
+        T.Abs(actual - expected) <= tolerance * T.Max(T.One, T.Abs(expected)) ? true
+            : throw new IsNotException(actual.Actually("is not close to", expected));
 }
