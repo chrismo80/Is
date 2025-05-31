580952b2b9998214ecaaee83ff65a3f9e2cf91cd
chrismo80 <krizm0@freenet.de>
2025-05-31 09:41:33 +0200

    Improve performance of list comarison assertions

--- Is/Assertions.cs
+++ Is/Assertions.cs
@@ -60,11 +60,11 @@ public static class Assertions
 
 	/// <summary>Asserts that the <paramref name="actual"/> sequence contains all the specified <paramref name="expected"/> elements.</summary>
 	public static bool IsContaining<T>(this IEnumerable<T> actual, params T[] expected) =>
-		expected.All(actual.Contains).ThrowIf(actual, "is not containing", expected);
+		actual.GetCountDiff(expected).All(c => c >= 0).ThrowIf(actual, "is not containing", expected);
 
 	/// <summary>Asserts that all elements in the <paramref name="actual"/> collection are present in the <paramref name="expected"/> collection.</summary>
 	public static bool IsIn<T>(this IEnumerable<T> actual, params T[] expected) =>
-		actual.All(expected.Contains).ThrowIf(actual, "is not in", expected);
+		expected.GetCountDiff(actual).All(c => c >= 0).ThrowIf(actual, "is not containing", expected);
 
 	/// <summary>Asserts that the <paramref name="actual"/> string contains the specified <paramref name="expected"/> substring.</summary>
 	public static bool IsContaining(this string actual, string expected) =>
@@ -86,15 +86,8 @@ public static class Assertions
 	}
 
 	/// <summary>Asserts that the <paramref name="actual"/> sequence matches the <paramref name="expected"/> sequence ignoring item order.</summary>
-	public static bool IsUnordered<T>(this IEnumerable<T> actual, IEnumerable<T> expected) where T : notnull
-	{
-		var counts = new Dictionary<T, int>();
-
-		counts.CountItems(expected, 1);
-		counts.CountItems(actual, -1);
-
-		return counts.Values.All(c => c == 0).ThrowIf(actual, "is not unordered", expected);
-	}
+	public static bool IsUnordered<T>(this IEnumerable<T> actual, IEnumerable<T> expected) where T : notnull =>
+		actual.GetCountDiff(expected).All(c => c == 0).ThrowIf(actual, "is not unordered", expected);
 
 	/// <summary>Asserts that the given synchronous <paramref name="action"/> throws an exception of type <typeparamref name="T"/> and that the exception message contains the specified <paramref name="message"/> substring.</summary>
 	public static bool IsThrowing<T>(this Action action, string message) where T : Exception =>
@@ -166,7 +159,17 @@ file static class Internals
 	internal static bool IsNear<T>(this T actual, T expected, T epsilon) where T : IFloatingPoint<T> =>
 		T.Abs(actual - expected) <= epsilon * T.Max(T.One, T.Abs(expected));
 
-	internal static void CountItems<T>(this Dictionary<T, int> dict, IEnumerable<T> source, int increment) where T : notnull
+	internal static List<int> GetCountDiff<T>(this IEnumerable<T> left, IEnumerable<T> right) where T : notnull
+	{
+		var counts = new Dictionary<T, int>();
+
+		counts.CountItems(left, 1);
+		counts.CountItems(right, -1);
+
+		return counts.Values.ToList();
+	}
+
+	private static void CountItems<T>(this Dictionary<T, int> dict, IEnumerable<T> source, int increment) where T : notnull
 	{
 		foreach (var item in source)
 			dict[item] = dict.GetValueOrDefault(item) + increment;
