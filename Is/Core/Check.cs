0a6392b2b4ac4850666e931de85e27166eb419ae
chrismo80 <krizm0@freenet.de>
2025-06-25 07:14:41 +0200

    Rename classes

--- Is/Core/Check.cs
+++ Is/Core/Check.cs
@@ -5,25 +5,25 @@ namespace Is.Core;
 [DebuggerStepThrough]
 public static class Check
 {
-	public static Failure<bool> That(bool condition) =>
+	public static Failable<bool> That(bool condition) =>
 		new(condition, true);
 
-	public static Result<TValue> That<TValue>(TValue value, Func<TValue, bool> predicate) =>
+	public static Returnable<TValue> That<TValue>(TValue value, Func<TValue, bool> predicate) =>
 		new(predicate(value), value);
 }
 
 [DebuggerStepThrough]
-public readonly struct Result<TValue>(bool condition, TValue value)
+public readonly struct Returnable<TValue>(bool condition, TValue value)
 {
-	public Failure<TResult> Yields<TResult>(Func<TValue, TResult> result) => condition switch
+	public Failable<TResult> Yields<TResult>(Func<TValue, TResult> result) => condition switch
 	{
-		true => new Failure<TResult>(true, result(value)),
-		false => new Failure<TResult>(false, default)
+		true => new Failable<TResult>(true, result(value)),
+		false => new Failable<TResult>(false, default)
 	};
 }
 
 [DebuggerStepThrough]
-public readonly struct Failure<TResult>(bool condition, TResult? result)
+public readonly struct Failable<TResult>(bool condition, TResult? result)
 {
 	public TResult? Unless(object? actual, string message, object? other) => condition switch
 	{
