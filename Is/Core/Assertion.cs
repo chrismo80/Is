819f8895718c0f3c56331e1d8e61a134636a9643
chrismo80 <krizm0@freenet.de>
2025-06-28 06:56:44 +0200

    Update Docu

--- Is/Core/Assertion.cs
+++ Is/Core/Assertion.cs
@@ -11,9 +11,9 @@ internal static class Assertion
 
 	internal static T Passed<T>(T result)
 	{
-		Configuration.Active.TestAdapter.ReportSuccess();
-
 		AssertionContext.Current?.AddSuccess();
+
+		Configuration.Active.TestAdapter.ReportSuccess();
 
 		return result;
 	}
@@ -38,22 +38,4 @@ internal static class Assertion
 
 	internal static T? Failed<T>(string message, List<string> text, int max = 100) =>
 		Failed<T>($"{message}\n\n\t{string.Join("\n\t", text.Truncate(max))}\n");
-}
-
-[DebuggerStepThrough]
-file static class StackFrameExtensions
-{
-	private static readonly Assembly Mine = Assembly.GetExecutingAssembly();
-
-	internal static StackFrame? FindFrame(this StackFrame[] frames) =>
-		frames.FirstOrDefault(f => f.IsExtensionCall() && f.GetFileName() != null);
-
-	private static bool IsExtensionCall(this StackFrame frame) =>
-		(frame.GetMethod()?.IsForeignAssembly() ?? false) && (frame.GetMethod()?.HasNotAttribute() ?? false);
-
-	private static bool IsForeignAssembly(this MethodBase method) =>
-		method.DeclaringType?.Assembly != Mine && !Attribute.IsDefined(method, typeof(IsAssertionAttribute));
-
-	private static bool HasNotAttribute(this MethodBase method) =>
-		!Attribute.IsDefined(method, typeof(IsAssertionAttribute)) && !Attribute.IsDefined(method.DeclaringType, typeof(IsAssertionsAttribute));
 }
--- Is/Core/ITestAdapter.cs
+++ Is/Core/ITestAdapter.cs
@@ -5,50 +5,36 @@ namespace Is.Core;
 /// <summary>
 /// Represents an interface for handling test result reporting.
 /// Serves as a hook for custom test frameworks to throw custom exception types.
+/// Provides a default implementation of that throws exceptions for test failures,
+/// specifically a <see cref="NotException"/> for single failures
+/// and a <see cref="AggregateException"/> for multiple failures.
 /// Can be set via Configuration.TestAdapter.
 /// </summary>
 public interface ITestAdapter
 {
 	/// <summary>Reports a successful test result to the configured test adapter.</summary>
-	void ReportSuccess();
+	void ReportSuccess() { }
 
 	/// <summary>Reports a failed test result to the configured test adapter.</summary>
-	void ReportFailure(Failure failure);
-
-	/// <summary>Reports multiple test failures to the configured test adapter.</summary>
-	void ReportFailures(string message, List<Failure> failures);
-}
-
-/// <summary>
-/// Provides a default implementation of the ITestAdapter interface.
-/// Throws exceptions for test failures, specifically NotException for single failures and AggregateException for multiple failures.
-/// </summary>
-public class DefaultTestAdapter : ITestAdapter
-{
-	public void ReportSuccess() { }
-
-	public void ReportFailure(Failure failure) =>
+	void ReportFailure(Failure failure) =>
 		throw new NotException(failure);
 
-	public void ReportFailures(string message, List<Failure> failures) =>
+	/// <summary>Reports multiple test failures to the configured test adapter.</summary>
+	void ReportFailures(string message, List<Failure> failures) =>
 		throw new AggregateException(message, failures.Select(f => new NotException(f)));
 }
 
 /// <summary>
-/// A test adapter implementation that suppresses specific output for test assertions.
-/// Provides minimal reporting behaviour by silencing failure messages.
-/// Serves as a silent alternative to more verbose test adapters.
+/// Default TestAdapter using the default implementation of the <see cref="ITestAdapter"/> interface
 /// </summary>
-public class SilentTestAdapter : ITestAdapter
+public class DefaultTestAdapter : ITestAdapter;
+
+/// <summary>
+/// <see cref="ITestAdapter"/> that simply logs the failures to the Console, does not throw any exceptions
+/// </summary>
+public class ConsoleTestAdapter : ITestAdapter
 {
-	public void ReportSuccess() { }
+	public void ReportFailure(Failure failure) => Console.WriteLine(failure.Message.RemoveLineBreaks());
 
-	public void ReportFailure(Failure failure) =>
-		Console.WriteLine(failure.Message.RemoveLineBreaks());
-
-	public void ReportFailures(string message, List<Failure> failures)
-	{
-		foreach (var failure in failures)
-			ReportFailure(failure);
-	}
+	public void ReportFailures(string message, List<Failure> failures) => failures.ForEach(ReportFailure);
 }
